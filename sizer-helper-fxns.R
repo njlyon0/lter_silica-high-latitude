528d9b226d75db17eb8f03e8b926da12eebc442f
njlyon0 <nicholasjlyon@gmail.com>
2022-08-12 12:21:35 -0400

    Moved `id_inflections` above the `sizer_lm` function

--- sizer-helper-fxns.R
+++ sizer-helper-fxns.R
@@ -253,8 +253,70 @@ sizer_ggplot <- function(raw_data = NULL, x = NULL, y = NULL,
   return(p)
 }
 
+# Function No. 6 - Identify Inflection Points in Slope -------------
+## Purpose: "Breaks" a given continuous variable at the inflection points identified by `sizer_aggregate` or `sizer_slice` and creates a column (named `groups`) that contains these group designations. Additionally, it identifies the start and end value of X for each group in two other new columns (named `start` and `end`, respectively)
+id_inflections <- function(raw_data = NULL, sizer_data = NULL,
+                           x = NULL, y = NULL){
+  
+  # Error out if these aren't provided
+  if(is.null(raw_data) | is.null(sizer_data) | is.null(x) | is.null(y))
+    stop("All arguments must be provided.")
+  
+  # Error out if the data are not both dataframes
+  if(class(raw_data) != "data.frame" | class(sizer_data) != "data.frame") 
+    stop("Both the raw data and the extracted SiZer data must be data frames")
+  
+  # Error out if the column names are not characters 
+  if(!is.character(x) | !is.character(y))
+    stop("The x and y columns must be specified as characters")
+  
+  # Error out if the column names are not in the data object
+  if(!x %in% names(raw_data) | !y %in% names(raw_data))
+    stop("`x` and `y` are not names in the provided `raw_data` object")
+  
+  # Grab inflection points
+  brk_pts <- c(sizer_data$pos_to_neg, sizer_data$neg_to_pos)
+  
+  # Drop NAs
+  brk_pts_actual <- brk_pts[!is.na(brk_pts)]
+  brk_pts_actual
+  
+  # Create necessary columns
+  data_mod <- raw_data %>%
+    # Identify rough groups
+    dplyr::mutate(
+      groups = base::cut(x = raw_data[[x]],
+                         breaks = c(-Inf, brk_pts_actual, Inf)),
+      .after = x) %>%
+    # Identify start / end years from the groups
+    tidyr::separate(col = groups, sep = ",", remove = FALSE,
+                    into = c('rough_start', 'rough_end')) %>%
+    # Remove parentheses / brackets
+    dplyr::mutate(
+      simp_start = stringr::str_sub(
+        rough_start, start = 2, end = nchar(rough_start)),
+      simp_end = gsub(pattern = "]| ", replacement = "", 
+                      x = rough_end)) %>%
+    # Swap "Inf" and "-Inf" for the actual start/end X values
+    dplyr::mutate(
+      start = as.numeric(ifelse(test = (simp_start == -Inf),
+                                yes = dplyr::first(raw_data[[x]]),
+                                no = simp_start)),
+      end = as.numeric(ifelse(test = (simp_end == Inf),
+                              yes = dplyr::last(raw_data[[x]]),
+                              no = simp_end)),
+      .after = groups) %>%
+    # Remove intermediary columns
+    dplyr::select(-rough_start, -rough_end,
+                  -simp_start, -simp_end) %>%
+    # Make it a dataframe
+    as.data.frame()
+  
+  # Return that modified dataframe
+  return(data_mod) }
+
 # Function No. 6 - Linear Models with SiZer ------------------------
-## Purpose: "Breaks" a given continuous variable at the inflection points identified by `sizer_aggregate` or `sizer_slice` and fits separate linear models for each "chunk" of the line between those inflection points. Returns a list containing (1) the model summary statistics and (2) the the estimates of the intercept and line
+## Purpose: fits separate linear models for each group of the line between the break points identified by `id_inflections` or `id_slope_changes`. Returns a list containing (1) the model summary statistics and (2) the the estimates of the intercept and line
 sizer_lm <- function(raw_data = NULL, x = NULL, y = NULL,
                      sizer_data = NULL){
   
@@ -362,67 +424,6 @@ sizer_lm <- function(raw_data = NULL, x = NULL, y = NULL,
     return(return_list) }
 }
 
-# Function No. 7 - Identify Inflection Points in Slope -------------
-
-id_inflections <- function(raw_data = NULL, sizer_data = NULL,
-                           x = NULL, y = NULL){
-  
-  # Error out if these aren't provided
-  if(is.null(raw_data) | is.null(sizer_data) | is.null(x) | is.null(y))
-    stop("All arguments must be provided.")
-  
-  # Error out if the data are not both dataframes
-  if(class(raw_data) != "data.frame" | class(sizer_data) != "data.frame") 
-    stop("Both the raw data and the extracted SiZer data must be data frames")
-  
-  # Error out if the column names are not characters 
-  if(!is.character(x) | !is.character(y))
-    stop("The x and y columns must be specified as characters")
-  
-  # Error out if the column names are not in the data object
-  if(!x %in% names(raw_data) | !y %in% names(raw_data))
-    stop("`x` and `y` are not names in the provided `raw_data` object")
-  
-  # Grab inflection points
-  brk_pts <- c(sizer_data$pos_to_neg, sizer_data$neg_to_pos)
-  
-  # Drop NAs
-  brk_pts_actual <- brk_pts[!is.na(brk_pts)]
-  brk_pts_actual
-  
-  # Create necessary columns
-  data_mod <- raw_data %>%
-    # Identify rough groups
-    dplyr::mutate(
-      groups = base::cut(x = raw_data[[x]],
-                         breaks = c(-Inf, brk_pts_actual, Inf)),
-      .after = x) %>%
-    # Identify start / end years from the groups
-    tidyr::separate(col = groups, sep = ",", remove = FALSE,
-                    into = c('rough_start', 'rough_end')) %>%
-    # Remove parentheses / brackets
-    dplyr::mutate(
-      simp_start = stringr::str_sub(
-        rough_start, start = 2, end = nchar(rough_start)),
-      simp_end = gsub(pattern = "]| ", replacement = "", 
-                      x = rough_end)) %>%
-    # Swap "Inf" and "-Inf" for the actual start/end X values
-    dplyr::mutate(
-      start = as.numeric(ifelse(test = (simp_start == -Inf),
-                                yes = dplyr::first(raw_data[[x]]),
-                                no = simp_start)),
-      end = as.numeric(ifelse(test = (simp_end == Inf),
-                              yes = dplyr::last(raw_data[[x]]),
-                              no = simp_end)),
-      .after = groups) %>%
-    # Remove intermediary columns
-    dplyr::select(-rough_start, -rough_end,
-                  -simp_start, -simp_end) %>%
-    # Make it a dataframe
-    as.data.frame()
-  
-  # Return that modified dataframe
-  return(data_mod) }
 
 
 
