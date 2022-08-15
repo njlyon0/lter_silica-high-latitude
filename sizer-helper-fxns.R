9a50ab97566ad37c54ee8dfba8e1f293fa2bd1bc
njlyon0 <nicholasjlyon@gmail.com>
2022-08-15 13:29:55 -0400

    Improved the `sizer_ggplot` function to allow selection of one of two trendlines (or no trendlines) and to allow selecting either, both, or neither type of inflection points

--- sizer-helper-fxns.R
+++ sizer-helper-fxns.R
@@ -194,8 +194,9 @@ sizer_plot <- function(sizer_object = NULL,
 # Function No. 5 - SiZer ggplot ------------------------------------
 ## Purpose: Creates a `ggplot2` plot of the trendline with slope changes identified by `SiZer` in dashed orange lines and inflection points (i.e., +/- or -/+ slope changes) identified in blue and red respectively
 sizer_ggplot <- function(raw_data = NULL, x = NULL, y = NULL,
-                         sizer_data = NULL, trendline = FALSE,
-                         vline = "all"){
+                         sizer_data = NULL, trendline = "none",
+                         vline = "all",
+                         sharp_colors = c("#DDDDDD", 'orange')){
   
   # Error out if these aren't provided
   if(is.null(raw_data) | is.null(sizer_data) |
@@ -215,41 +216,39 @@ sizer_ggplot <- function(raw_data = NULL, x = NULL, y = NULL,
   if(!x %in% names(raw_data) | !y %in% names(raw_data))
     stop("`x` and `y` are not names in the provided `raw_data` object")
   
+  # Error out for unsupported trendline call
+  if(!trendline %in% c("smooth", "sharp", "none", FALSE))
+    stop("`trendline` must be one of 'smooth', 'sharp', or 'none'")
+  
   # Error out for unsupported vertical intercept call
-  if(!vline %in% c("all", "inflections", "changes", "none"))
+  if(!vline %in% c("all", "inflections", "changes", "none", FALSE))
     stop("`vline` must be one of 'all', 'inflections', 'changes', or 'none'")
   
-  # Warning if trendline isn't TRUE/FALSE
-  if(class(trendline) != "logical"){
-    message("`trendline` must be TRUE or FALSE. Defaulting to FALSE")
-    trendline <- FALSE }
+  # Error out if trendline is 'sharp' but color length is wrong
+  if(trendline == "sharp" & length(sharp_colors) != 2)
+    stop("`sharp_colors` must be a vector of two elements. The first is the color for the flat slope segments and the second is the color for the increasing/decreasing segments")
+  
+  # Warning if trendline is FALSE (rather than "none")
+  if(trendline == FALSE){
+    message("`trendline` should be set to 'none' if no trendline is desired. Defaulting to 'none'")
+    trendline <- "none" }
   
   # Warning if vline is set to FALSE (rather than "none")
   if(vline == FALSE){
     message("`vline` should be set to 'none' if no x-intercepts are desired. Defaulting to 'none'")
-    vline <- "none"
-  }
+    vline <- "none" }
   
-  # Now make the actual plot
+  # Make the foundations of the actual plot
   p <- ggplot(data = raw_data, aes_string(x = x, y = y)) +
-    geom_point() +
     theme_classic()
-  
+
   # Add x-intercepts for slope changes if desired
-  
-  # Including SiZer-identified inflection points
   if(vline %in% c("all", "changes")){
   p <- p + 
     geom_vline(xintercept = sizer_data$mean_x, color = 'orange',
              linetype = 2, na.rm = TRUE) +
     geom_vline(xintercept = sizer_data$x_grid, color = 'orange',
                linetype = 2, na.rm = TRUE) }
-  
-  # If `trendline` is TRUE, add the trendline
-  if(trendline == TRUE) {
-  p <- p +
-    geom_smooth(method = 'loess', formula = 'y ~ x', 
-                se = FALSE, color = 'black') }
   
   # Add the positive to negative inflection point line(s) if one exists and they are desired
   if(!base::all(is.na(sizer_data$pos_to_neg)) & vline %in% c("all", "inflections")){
@@ -262,6 +261,24 @@ sizer_ggplot <- function(raw_data = NULL, x = NULL, y = NULL,
     p <- p +
       geom_vline(xintercept = sizer_data$neg_to_pos, color = 'red',
                  na.rm = TRUE) }
+  
+  # Add the points to the plot (we want them "in front of" the vertical lines but "behind" the trendlines)
+  p <- p +
+    geom_point()
+  
+  # If `trendline` is 'smooth', add that trendline
+  if(trendline == "smooth") {
+    p <- p +
+      geom_smooth(method = 'loess', formula = 'y ~ x', 
+                  se = FALSE, color = 'black') }
+  
+  # If `trendline` is 'sharp', add that trendline
+  if(trendline == "sharp"){
+    p <- p +
+      geom_smooth(aes(group = groups, color = slope_type),
+                  method = lm, formula = 'y ~ x', se = FALSE) +
+      scale_color_manual(values = sharp_colors) +
+      theme(legend.position = 'none') }
   
   # Return the plot
   return(p)
