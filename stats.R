ca2bfc0130196b9d20be22c9321a709f6659c75a
njlyon0 <nicholasjlyon@gmail.com>
2023-09-12 10:33:14 -0400

    Fleshed out preliminary Si concentration analysis with RRPP

--- stats.R
+++ stats.R
@@ -26,7 +26,32 @@ dir.create(path = file.path("stats_results"), showWarnings = F)
 rm(list = ls())
 
 ## ----------------------------------------- ##
-# Silica Concentration ----
+          # Custom Function(s) ----
+## ----------------------------------------- ##
+
+# Neat little tool to summarize key bits of aov table
+aov_process <- function(fit){
+  
+  # Do ANOVA table
+  mod_aov <- anova(fit)
+  
+  # Get the summary table
+  mod_table <- as.data.frame(mod_aov$table)
+  
+  # Get the terms out as a real column (default is as rownames)
+  mod_out <- mod_table %>%
+    dplyr::mutate(Term = rownames(.), .before = dplyr::everything()) %>%
+    # Rename P value while we're here
+    dplyr::rename(P_Value = `Pr(>F)`)
+  
+  # Drop the rownames
+  rownames(mod_out) <- NULL
+  
+  # Return that last object
+  return(mod_out) }
+
+## ----------------------------------------- ##
+          # Silica Concentration ----
 ## ----------------------------------------- ##
 # Read in ready data for this response variable
 si_conc <- read.csv(file = file.path("tidy_data", "stats-ready_annual_Conc_uM_DSi_bw5.csv"))
@@ -34,8 +59,23 @@ si_conc <- read.csv(file = file.path("tidy_data", "stats-ready_annual_Conc_uM_DS
 # Check structure
 dplyr::glimpse(si_conc)
 
+# Fit a model of interest
+si_conc_mod1 <- RRPP::lm.rrpp(slope_estimate ~ LTER +
+                                ## Dynamic drivers
+                                slope_temp_degC + slope_precip_mm.per.day +
+                                slope_npp_kg.C.m2.year + slope_evapotrans_kg.m2 +
+                                slope_snow_max.prop.area + slope_snow_num.days +
+                                ## Static drivers
+                                Latitude + elevation_mean_m + land_total_forest +
+                                land_barren_or_sparsely_vegetated,
+                              data = si_conc, iter = 999)
 
+# Summarize that output (and check it out)
+( si_conc_aov1 <- aov_process(si_conc_mod1) )
 
+# Export locally
+write.csv(x = si_conc_aov1, row.names = F, na = '',
+          file = file.path("stats_results", "annual_DSi_conc.csv"))
 
 
 
