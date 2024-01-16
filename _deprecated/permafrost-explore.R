fbdb0c5b3a348a65fa6a6be97b4335509d64d3c5
njlyon0 <nicholasjlyon@gmail.com>
2024-01-16 13:10:02 -0500

    Start of permafrost exploration script

---
+++ _deprecated/permafrost-explore.R
@@ -0,0 +1,22 @@
+## ------------------------------------------ ##
+            # Permafrost Exploration
+## ------------------------------------------ ##
+# Written by: Nick J Lyon
+
+# PURPOSE
+## Explore permafrost data options (before building them into the site map)
+
+## ------------------------------------------ ##
+            # Housekeeping ----
+## ------------------------------------------ ##
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(googledrive, tidyverse, readxl, sf, maps, terra, supportR, cowplot)
+
+# Clear environment & collect garbage
+rm(list = ls()); gc()
+
+
+
+
+# End ----
