99315bbf09610320545387da70b54af3c6fa6f17
njlyon0 <nicholasjlyon@gmail.com>
2024-01-16 13:17:59 -0500

    Very preliminary exploration of a candidate permafrost data object

--- _deprecated/permafrost-explore.R
+++ _deprecated/permafrost-explore.R
@@ -11,10 +11,30 @@
 ## ------------------------------------------ ##
 # Load libraries
 # install.packages("librarian")
-librarian::shelf(googledrive, tidyverse, readxl, sf, maps, terra, supportR, cowplot)
+librarian::shelf(googledrive, tidyverse, sf)
 
 # Clear environment & collect garbage
 rm(list = ls()); gc()
+
+## ------------------------------------------ ##
+     # Circum-Arctic Map of Permafrost ----
+## ------------------------------------------ ##
+# Name: Circum-Arctic Map of Permafrost and Ground-Ice Conditions, Version 2
+## Source: https://nsidc.org/data/ggd318/versions/2
+
+# Read in this file
+pf <- sf::st_read(dsn = file.path("permafrost_test", "permaice.shp"))
+
+# Check various structural elements
+sf::st_crs(x = pf)
+
+# Exploratory plot
+## NOTE: takes a few minutes to load
+plot(pf)
+
+
+
+
 
 
 
