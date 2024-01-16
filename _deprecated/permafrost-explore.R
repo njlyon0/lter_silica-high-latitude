6a41014ab902dc5d02e385667cd12faf0cc9cc03
njlyon0 <nicholasjlyon@gmail.com>
2024-01-16 14:29:55 -0500

    Added another variant to explore (likely 'winner' for the needs of the map!)

--- _deprecated/permafrost-explore.R
+++ _deprecated/permafrost-explore.R
@@ -17,10 +17,39 @@ rm(list = ls()); gc()
 librarian::shelf(googledrive, tidyverse, sf, ncdf4, terra)
 
 ## ------------------------------------------ ##
-    # Permafrost Extent & Ground Temp ----
+    # Permafrost Extent (1km raster) ----
 ## ------------------------------------------ ##
 # Name: Permafrost Extent and Ground Temperature Map, 2000-2016, Northern Hemisphere Permafrost
 ## Source: https://apgc.awi.de/dataset/pex
+
+# Read in the raster
+pf_rast <- terra::rast(x = file.path("permafrost_test", "UiO_PEX_PERPROB_5.0_20181128_2000_2016_NH", "UiO_PEX_PERPROB_5.0_20181128_2000_2016_NH.tif"))
+
+# Check layers
+names(pf_rast)
+
+# Check structure
+print(pf_rast)
+
+# Re-project into desired CRS
+## Note takes ~5 minutes to complete
+pf_wgs84 <- terra::project(x = pf_rast, y = "epsg:4326")
+
+## Export this locally for easier re-use
+# terra::writeRaster(x = pf_wgs84, overwrite = T, filename = file.path("permafrost_test", "perprob.tif"))
+
+# Exploratory plot (of permafrost probability)
+terra::plot(pf_wgs84, axes = T)
+
+# Re-clear environment
+rm(list = ls())
+
+## ------------------------------------------ ##
+    # Permafrost Extent (25km netCDF) ----
+## ------------------------------------------ ##
+# Name: Permafrost Extent and Ground Temperature Map, 2000-2016, Northern Hemisphere Permafrost
+## Source: https://apgc.awi.de/dataset/pex
+## NetCDF Resampled from 1km to 25km resolution
 
 # Read in this file
 pf_nc <- ncdf4::nc_open(filename = file.path("permafrost_test", "UiO_PEX_5.0_20181127_2000_2016_25km.nc"))
@@ -48,7 +77,10 @@ pf_wgs84 <- terra::project(x = pf_prob, y = "epsg:4326")
 
 # Exploratory plot (of permafrost probability)
 terra::plot(pf_wgs84, axes = T)
+## Looks like projection is wrong but the hard-coded one is the only one listed in the data doc
 
+# Re-clear environment
+rm(list = ls())
 
 ## ------------------------------------------ ##
      # Circum-Arctic Map of Permafrost ----
@@ -57,15 +89,17 @@ terra::plot(pf_wgs84, axes = T)
 ## Source: https://nsidc.org/data/ggd318/versions/2
 
 # Read in this file
-pf <- sf::st_read(dsn = file.path("permafrost_test", "permaice.shp"))
+pf_sf <- sf::st_read(dsn = file.path("permafrost_test", "permaice.shp"))
 
 # Check various structural elements
-sf::st_crs(x = pf)
+sf::st_crs(x = pf_sf)
 
 # Exploratory plot
 ## NOTE: waited 10 minutes and it doesn't work (plus crashed local RStudio)
 ## Need to explore alternate options for visualization
 # plot(pf)
 
+# Re-clear environment
+rm(list = ls())
 
 # End ----
