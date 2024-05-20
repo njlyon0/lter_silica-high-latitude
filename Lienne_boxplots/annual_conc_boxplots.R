93dff30f3cc3f98d1bdcd488257e894140cd034b
Lienne Sethna <30530947+lsethna@users.noreply.github.com>
2024-05-20 13:02:59 -0500

    Create facet plots for each site's annual WRTDS results

--- Lienne_boxplots/annual_conc_boxplots.R
+++ Lienne_boxplots/annual_conc_boxplots.R
@@ -36,24 +36,73 @@ high_lat_annual <- annual %>% filter(LTER %in% high_lat_lter)
 unique(high_lat_annual$LTER) #where is Swedish Government data?
 unique(high_lat_annual$chemical)
 
-#add column for fills for consistency between plots
-high_lat_annual <-
-high_lat_annual %>% mutate(LTER_fill = case_when(LTER=="Canada"~"#003f5c",
-                                                 LTER=="Finnish Environmental Institute"~"#374c80",
-                                                 LTER=="GRO"~"#7a5195",
-                                                 LTER=="Krycklan"~"#bc5090",
-                                                 LTER=="MCM"~"#ef5675",
-                                                 LTER=="NIVA"~"#ff764a",
-                                                 LTER=="Swedish Government"~"#ffa600"))
+#clean site names
+high_lat_annual <- high_lat_annual %>%
+  #change to lower case
+  mutate(plot_site_name = tolower(Stream_Name)) %>%
+  #only keep text before "stream" or "river"
+  mutate(plot_site_name = gsub("(stream|river|creek).*","",plot_site_name)) %>%
+  #change to sentence case
+  mutate(plot_site_name = str_to_title(plot_site_name))
+
+#create df of fills for consistency between plots
+high_lat_annual_colors <- high_lat_annual %>%
+  mutate(LTER_fill = case_when(LTER=="Canada"~"#003f5c",
+                               LTER=="Finnish Environmental Institute"~"#374c80",
+                               LTER=="GRO"~"#7a5195",
+                               LTER=="Krycklan"~"#bc5090",
+                               LTER=="MCM"~"#ef5675",
+                               LTER=="NIVA"~"#ff764a",
+                               LTER=="Swedish Government"~"#ffa600")) %>%
+  select(plot_site_name,LTER_fill) %>%
+  distinct()
 
 ## ---------------------------------------- ##
 ## Boxplots!
 ## ---------------------------------------- ##
+dsi <- 
 high_lat_annual %>% filter(chemical=="DSi") %>%
-  filter(LTER=="Canada") %>%
-  ggplot(aes(x=Stream_Name,y=Conc_uM,fill=LTER_fill))+
-  geom_boxplot()+
-  scale_fill_manual(values=high_lat_annual$LTER_fill)+
-  theme(axis.text.x=element_text(angle=90),
-        legend.position="none")+
-  facet_grid(~LTER)
+  ggplot()+
+  geom_boxplot(aes(x=plot_site_name,y=Conc_uM,fill=LTER),width=1)+
+  scale_fill_manual(values=c("#003f5c","#374c80","#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"))+
+  scale_y_continuous(limits=c(0,300),name="DSi concentration (uM)")+
+  facet_grid(~LTER,scales="free_x",space="free")+ #switch="x"; will move facet labels under x-axis
+  theme_classic(base_size=14)+
+  theme(axis.text.x=element_text(angle=45,hjust=1,size=10),
+        legend.position="none",
+        axis.title.x=element_blank(),
+        strip.placement="outside",
+        strip.background=element_blank())
+
+din <- 
+  high_lat_annual %>% filter(chemical=="DIN") %>%
+  #add placeholder data for missing Canada sites?
+  ggplot()+
+  geom_boxplot(aes(x=plot_site_name,y=Conc_uM,fill=LTER),width=1)+
+  scale_fill_manual(values=c("#003f5c","#374c80","#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"))+
+  scale_y_continuous(limits=c(0,250),name="DIN concentration (uM)")+
+  facet_grid(~LTER,scales="free_x",space="free")+
+  theme_classic(base_size=14)+
+  theme(axis.text.x=element_text(angle=45,hjust=1,size=10),
+        legend.position="none",
+        axis.title.x=element_blank(),
+        strip.placement="outside",
+        strip.background=element_blank())
+
+dip <- 
+  high_lat_annual %>% filter(chemical=="P") %>%
+  #add placeholder data for missing Canada sites?
+  ggplot()+
+  geom_boxplot(aes(x=plot_site_name,y=Conc_uM,fill=LTER),width=1)+
+  scale_fill_manual(values=c("#003f5c","#374c80","#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"))+
+  scale_y_continuous(limits=c(0,2.5),name="DIP concentration (uM)")+
+  facet_grid(~LTER,scales="free_x",space="free")+
+  theme_classic(base_size=14)+
+  theme(axis.text.x=element_text(angle=45,hjust=1,size=10),
+        legend.position="none",
+        axis.title.x=element_blank(),
+        strip.placement="outside",
+        strip.background=element_blank())
+
+#patchwork together
+dsi/din/dip
