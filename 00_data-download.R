aff142fbc5d1bfb23c77b9dd64a64c3e017991a9
njlyon0 <nicholasjlyon@gmail.com>
2024-07-19 09:37:22 -0400

    First pass at fleshed-out data download script

--- 00_data-download.R
+++ 00_data-download.R
@@ -11,9 +11,79 @@
 ## It will not work if you don't have prior access
 
 ## ----------------------------------------- ##
-# Housekeeping ----
+              # Housekeeping ----
 ## ----------------------------------------- ##
 
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, googledrive)
 
+# Clear environment
+rm(list = ls())
+
+# Make needed folder(s)
+dir.create(path = file.path("data"), showWarnings = F)
+
+## ----------------------------------------- ##
+        # WRTDS Outputs Download ----
+## ----------------------------------------- ##
+
+# Identify the WRTDS outputs folder
+wrtds_files <- googledrive::drive_ls(path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1V5EqmOlWA8U9NWfiBcWdqEH9aRAP-zCk")) %>% 
+  # And pare down the outputs to only desired ones
+  dplyr::filter(name %in% c("Full_Results_WRTDS_annual.csv", "Full_Results_WRTDS_monthly.csv"))
+
+# Check them
+wrtds_files
+
+# Download those files
+purrr::walk2(.x = wrtds_files$id, .y = wrtds_files$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                                path = file.path("data", .y)))
+
+# Clear environment
+rm(list = ls())
+
+## ----------------------------------------- ##
+      # Spatial Drivers Download ----
+## ----------------------------------------- ##
+
+# Identify driver file(s)
+driver_outs <- googledrive::drive_ls(path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1FBq2-FW6JikgIuGVMX5eyFRB6Axe2Hld")) %>% 
+  # Filter to only desired file (omitting date stamp in file name for flexibility)
+  dplyr::filter(stringr::str_detect(string = name, pattern = "all-data_si-extract_2_"))
+
+# Check 'em
+driver_outs
+
+# Create a simplified file name (lacking date stamp)
+driver_name <- "all-data_si-extract_2.csv"
+
+# Download that file
+purrr::walk2(.x = driver_outs$id, .y = driver_outs$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                                path = file.path("data", driver_name)))
+
+# Clear environment
+rm(list = ls())
+
+## ----------------------------------------- ##
+        # Reference Table Download ----
+## ----------------------------------------- ##
+
+# Identify ref. table file
+ref_out <- googledrive::drive_ls(path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/0AIPkWhVuXjqFUk9PVA")) %>%
+  dplyr::filter(name == "Site_Reference_Table")
+
+# Check it
+ref_out
+
+# Download it
+purrr::walk2(.x = ref_out$id, .y = ref_out$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                                path = file.path("data", .y)))
+
+# Clear environment
+rm(list = ls())
 
 # End ----
