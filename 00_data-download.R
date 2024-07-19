d55a30674f7d7ba3b0fb9fbf1d933e64c21aa421
njlyon0 <nicholasjlyon@gmail.com>
2024-07-19 09:16:14 -0400

    Starting dedicated 'data download' script

---
+++ 00_data-download.R
@@ -0,0 +1,19 @@
+## ------------------------------------------------------- ##
+                 # Download / Retrieve Data
+## ------------------------------------------------------- ##
+# Written by: Nick J Lyon & Joanna Carey
+
+# Purpose:
+## Identify & download data from Google Drive
+
+# IMPORTANT CAVEAT:
+## This script requires access to the "LTER-WG_Silica-Export" Shared Drive
+## It will not work if you don't have prior access
+
+## ----------------------------------------- ##
+# Housekeeping ----
+## ----------------------------------------- ##
+
+
+
+# End ----
