9d701a101046d53c439be32bb1bb3f76abc79c21
njlyon0 <nicholasjlyon@gmail.com>
2024-07-22 14:35:57 -0400

    Empty script for a bookmark graph function

---
+++ tools/fxn_bookmark-graph.R
@@ -0,0 +1,17 @@
+#' @ title Make a Bookmark Graph
+#' 
+#' @ description
+#' 
+#' @ param data
+#' @ param facet_var
+#' @ param colors
+#'
+#'
+bookmark_graph <- function(data, facet_var, colors){
+  
+  
+  
+  
+}
+
+# End ----
