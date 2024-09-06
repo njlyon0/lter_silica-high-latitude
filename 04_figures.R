fa13abe863f654057ee569365b1b69db8d9b9451
njlyon0 <nicholasjlyon@gmail.com>
2024-09-06 17:38:04 -0400

    dev: partial integration of 'discharge bookmark' figure process into 'chemical bookmark'

--- 04_figures.R
+++ 04_figures.R
@@ -111,7 +111,7 @@ ggsave(filename = file.path("figures", "fig_bookmark-discharge.png"),
 rm(list = c("streams_per_lter", "df_q", "df_q_simp"))
 
 ## ----------------------------------------- ##
-# Data Prep ----
+        # Chemical Bookmark Figures ----
 ## ----------------------------------------- ##
 
 # Identify conserved file stem
@@ -128,44 +128,87 @@ df_si.n<- read.csv(file = file.path("data", paste0(file_stem, file_resp, "_Si_DI
 df_si.p <- read.csv(file = file.path("data", paste0(file_stem, file_resp, "_Si_P.csv")))
 
 # Combine into a single object that loses nothing
-df_all <- dplyr::bind_rows(df_si, df_n, df_p, df_si.n, df_si.p)
+df_chem_all <- dplyr::bind_rows(df_si, df_n, df_p, df_si.n, df_si.p)
 
-# Check structure
-dplyr::glimpse(df_all)
-
-# Pare down to just needed rows/columns
-df_simp <- df_all %>% 
-  # Arrange by LTER and site
+# Wrangle this like we wrangled discharge (see above)
+df_chem_simp <- df_chem_all %>% 
   dplyr::arrange(LTER, Stream_Name) %>%
-  # Do some final tweaks
   dplyr::mutate(
-    ## Fix slope direction categories
     slope_direction = dplyr::case_when(
       significance == "NS" ~ "NS",
       significance == "marg" ~ "NS",
       is.na(slope_direction) == T ~ "NA",
       T ~ slope_direction),
-    ## Assign level order
     slope_direction = factor(slope_direction, levels = c("pos", "neg", "NS", "NA")), 
-    ## Bin duration
     duration_bin = ifelse(test = (section_duration <= 5),
                           yes = "short", no = "long") ) %>% 
-  # Pare down to only needed columns
   dplyr::select(sizer_groups, LTER, Stream_Name, LTER_stream, chemical, Year, 
                 dplyr::all_of(x = file_resp), significance, slope_direction, duration_bin) %>%
-  # Drop non-unique rows
   dplyr::distinct()
 
 # Re-check structure
-dplyr::glimpse(df_simp)
+dplyr::glimpse(df_chem_simp)
 
 # Count streams / LTER
-(streams_per_lter <- lter_ct(data = df_simp))
+(streams_per_lter <- lter_ct(data = df_chem_simp))
 
 # SUSBET FOR TESTING PURPOSES
 chem <- "DSi"
-TEST <- dplyr::filter(.data = df_simp, chemical == chem)
+TEST <- dplyr::filter(.data = df_chem_simp, chemical == chem)
 glimpse(TEST)
+
+
+
+
+ggplot(data = TEST, mapping = aes(x = Year, y = LTER_stream, color = slope_direction)) +
+  # Add paths for 'long' duration SiZer chunks
+  geom_path(data = TEST[TEST$duration_bin == "long", ], 
+            mapping = aes(group = sizer_groups), 
+            lwd = 3.5, lineend = 'square') +
+  # Add *semi-transparent* paths for short duration chunks
+  geom_path(data = TEST[TEST$duration_bin == "short", ], 
+            mapping = aes(group = sizer_groups), 
+            lwd = 3.5, lineend = 'square', alpha = 0.5) +
+  # Manually define colors
+  scale_color_manual(values = dir_palt) +
+  # Add lines between streams from different LTERs
+  geom_hline(yintercept = streams_per_lter$line_positions) +
+  ## Add LTER-specific annotations
+  geom_text(x = 1992, y = 1.5, label = "Canada", color = "black", hjust = "left") + 
+  annotate(geom = "text", x = 1993, color = "black", angle = 90, hjust = "center",
+           y = c(14, 27.5, 35.5, 46.5, 56.5, 69), 
+           label = c("Finnish Environ. Institute", "GRO", "Krycklan", "McMurdo", "NIVA", "Swedish Gov't")) +
+  # Customize labels and axis titles
+  labs(x = "Year", y = "Stream", 
+       title = paste0(chem, " ", gsub(pattern = "_", replacement = " (", x = file_resp), ")")) +
+  # Modify theme elements for preferred aesthetics
+  guides(color = guide_legend(override.aes = list(alpha = 1))) +
+  theme(panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        axis.line = element_line(color = "black"),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_blank(),
+        legend.background = element_blank(),
+        legend.position = "inside",
+        legend.position.inside = c(0.425, 0.88))
+
+# Export graph
+ggsave(filename = file.path("figures", "fig_bookmark-chemicals.png"),
+       height = 8, width = 5, units = "in")
+
+
+
+
+
+# Remove some items from the environment
+# rm(list = c("streams_per_lter", "df_q", "df_q_simp"))
+
+
+
+# BASEMENT ----
+
+
 
 # Create a bookmark graph for line direction alone (but only when significant)
 ggplot(data = TEST, mapping = aes(x = Year, y = LTER_stream, color = slope_direction)) +
@@ -196,7 +239,6 @@ ggsave(filename = file.path("figures", "bookmark_figure.png"),
 
 
 
-# BASEMENT ----
 
   
   
