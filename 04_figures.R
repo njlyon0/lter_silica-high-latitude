1c24d6fb7ae08f1e76d9eb486c00d3781a313502
njlyon0 <nicholasjlyon@gmail.com>
2024-09-06 17:25:52 -0400

    feat: discharge bookmark figure

--- 04_figures.R
+++ 04_figures.R
@@ -35,6 +35,73 @@ rm(list = "fxn")
 source(file.path("tools", "flow_graph-helpers.R"))
 
 ## ----------------------------------------- ##
+        # Discharge Bookmark Figure ----
+## ----------------------------------------- ##
+
+# Read in discharge data
+df_q <- read.csv(file = file.path("data", "stats-ready_annual_Discharge_cms_DSi.csv"))
+
+# Simplify this object
+df_q_simp <- df_q %>% 
+  # Arrange by LTER and site
+  dplyr::arrange(LTER, Stream_Name) %>%
+  # Do some final tweaks
+  dplyr::mutate(
+    ## Fix slope direction categories
+    slope_direction = dplyr::case_when(
+      significance == "NS" ~ "NS",
+      significance == "marg" ~ "NS",
+      is.na(slope_direction) == T ~ "NA",
+      T ~ slope_direction),
+    ## Assign level order
+    slope_direction = factor(slope_direction, levels = names(dir_palt)), 
+    ## Bin duration
+    duration_bin = ifelse(test = (section_duration <= 5),
+                          yes = "short", no = "long") ) %>% 
+  # Pare down to only needed columns
+  dplyr::select(sizer_groups, LTER, Stream_Name, LTER_stream, chemical, Year, 
+                Discharge_cms, significance, slope_direction, duration_bin) %>%
+  # Drop non-unique rows
+  dplyr::distinct()
+
+# Check structure
+dplyr::glimpse(df_q_simp)
+
+# Count streams / LTER
+(streams_per_lter <- lter_ct(data = df_q_simp))
+
+# Create a bookmark graph for line direction alone (but only when significant)
+ggplot(data = df_q_simp, mapping = aes(x = Year, y = LTER_stream, color = slope_direction)) +
+  geom_path(data = df_q_simp[df_q_simp$duration_bin == "long", ], 
+            mapping = aes(group = sizer_groups), 
+            lwd = 3.5, lineend = 'square') +
+  geom_path(data = df_q_simp[df_q_simp$duration_bin == "short", ], 
+            mapping = aes(group = sizer_groups), 
+            lwd = 3.5, lineend = 'square', alpha = 0.5) +
+  scale_color_manual(values = dir_palt) +
+  geom_hline(yintercept = streams_per_lter$line_positions) +
+  ## Add LTER-specific annotations
+  geom_text(x = 1992, y = 1.5, label = "Canada", color = "black", hjust = "left") + 
+  annotate(geom = "text", x = 1993, color = "black", angle = 90, hjust = "center",
+           y = c(14, 27.5, 35.5, 46.5, 56.5, 69), 
+           label = c("Finnish Environ. Institute", "GRO", "Krycklan", "McMurdo", "NIVA", "Swedish Gov't")) +
+  labs(x = "Year", y = "Stream", title = "Discharge (cms)") +
+  guides(color = guide_legend(override.aes = list(alpha = 1))) +
+  theme(panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        axis.line = element_line(color = "black"),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_blank(),
+        legend.background = element_blank(),
+        legend.position = "inside",
+        legend.position.inside = c(0.425, 0.88))
+
+# Export graph
+ggsave(filename = file.path("figures", "fig_bookmark-discharge.png"),
+       height = 8, width = 5, units = "in")
+
+## ----------------------------------------- ##
 # Data Prep ----
 ## ----------------------------------------- ##
 
@@ -65,6 +132,7 @@ df_simp <- df_all %>%
   dplyr::mutate(
     ## Fix slope direction categories
     slope_direction = dplyr::case_when(
+      significance == "NS" ~ "NS",
       significance == "marg" ~ "NS",
       is.na(slope_direction) == T ~ "NA",
       T ~ slope_direction),
@@ -82,30 +150,40 @@ df_simp <- df_all %>%
 # Re-check structure
 dplyr::glimpse(df_simp)
 
+# Count streams / LTER
+(streams_per_lter <- lter_ct(data = df_simp))
+
 # SUSBET FOR TESTING PURPOSES
 chem <- "DSi"
 TEST <- dplyr::filter(.data = df_simp, chemical == chem)
 glimpse(TEST)
 
-# Create a bookmark graph for line direction + P value
+# Create a bookmark graph for line direction alone (but only when significant)
 ggplot(data = TEST, mapping = aes(x = Year, y = LTER_stream, color = slope_direction)) +
   geom_path(data = TEST[TEST$duration_bin == "long", ], mapping = aes(group = sizer_groups), 
             lwd = 3.5, lineend = 'square') +
   geom_path(data = TEST[TEST$duration_bin == "short", ], mapping = aes(group = sizer_groups), 
-            lwd = 3.5, lineend = 'square', alpha = 0.2) +
+            lwd = 3.5, lineend = 'square', alpha = 0.4) +
   scale_color_manual(values = dir_palt) +
-  geom_hline(yintercept = lter_ct(data = TEST)$line_positions) +
+  geom_hline(yintercept = streams_per_lter$line_positions) +
+  ## Add LTER-specific annotations
+  geom_text(x = 1994, y = 1.5, label = "Can.", color = "black", hjust = "center") + 
+  annotate(geom = "text", x = 1993.5, color = "black", angle = 90, hjust = "center",
+           y = c(14, 27.5, 35.5, 46.5, 56.5, 69), 
+           label = c("Finnish Environ. Institute", "GRO", "Krycklan", "McMurdo", "NIVA", "Swedish Gov't")) +
   labs(x = "Year", y = "Stream") +
   theme(panel.background = element_blank(),
         axis.line = element_line(color = "black"),
         axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
         legend.title = element_blank(),
         legend.background = element_blank(),
         legend.position = "inside",
-        legend.position.inside = c(0.1, 0.9))
+        legend.position.inside = c(0.2, 0.9))
 
-
-
+# Export graph
+ggsave(filename = file.path("figures", "bookmark_figure.png"),
+       height = 8, width = 7, units = "in")
 
 
 
