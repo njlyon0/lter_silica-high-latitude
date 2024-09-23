4e5517de61536ffdb2e3ae47867ac2bbbaf1f581
Joanna Carey <jcarey@babson.edu>
2024-09-23 13:58:58 -0400

    tiny clean up - removed housekeeping

--- Jo_explore/AnnualBookmarkGraph_Summaries.r
+++ Jo_explore/AnnualBookmarkGraph_Summaries.r
@@ -19,9 +19,6 @@
 # install.packages("librarian")
 librarian::shelf(tidyverse, googledrive, cowplot)
 
-# Make a folder for exporting graphs
-dir.create(path = file.path("graphs"), showWarnings = F)
-
 # Clear environment
 rm(list = ls())
 
@@ -30,7 +27,7 @@ rm(list = ls())
 ## ----------------------------------------- ##
 
 # Grab the desired data file
-full_df <- read.csv(file = file.path("tidy_data", "stats-ready_nodriversannual_Conc_uM_DIN_bw5.csv")) 
+full_df <- read.csv(file = file.path("stats_ready_annualfromNick", "stats-ready_annual_Conc_uM_DSi.csv")) 
 
 full_df<-full_df %>%
   # Make both 'direction + X' columns into factors so we can pick an informative order
@@ -41,9 +38,6 @@ full_df<-full_df %>%
                                             "neg-bad", "neg-fine", "neg-good", "neg-great", 
                                             "NA", "NS")))
 
-# Check structure
-dplyr::glimpse(full_df)
-
 #create a short stream name column
 full_df$streamshort<-paste0(stringr::str_sub(full_df$LTER,1,3),"_",
                                 paste0(stringr::str_sub(full_df$Stream_Name,1,20)))
@@ -53,7 +47,7 @@ core_df <- full_df %>%
   # Arrange by LTER and site
   dplyr::arrange(LTER, Stream_Name) %>%
   # Pare down to only needed columns
-  dplyr::select(sizer_groups, LTER, Stream_Name, stream, chemical:section_duration, 
+  dplyr::select(sizer_groups, LTER, Stream_Name, chemical:section_duration, 
                 F_statistic:line_fit, slope_estimate:slope_std_error,
                 dplyr::starts_with("dir_"), streamshort) %>%
   # Drop non-unique rows
@@ -76,33 +70,31 @@ sig_only <- core_df %>%
   # Drop non-unique rows
   dplyr::distinct()
 
-# Check it out
-dplyr::glimpse(sig_only)
-
 
 #================================================
 #across all years all rivers, how many yrs of inc? dec? 
 #for each network, how many yrs of increasing and decreasing?
-names(core_df)
 
 #if want marginal significant too then do this on the core_df
 
-sig_only %>%
+SiZerOuts_Summary_Direction <- sig_only %>%
   dplyr::group_by(LTER, dir_sig) %>% #add in stream if want to do it by stream
   dplyr::summarize(ct=n(), 
                    avg_perChange = mean(percent_change),
-                   sd_perChange = sd(percent_change)) #not working b/c this column are factors?
-  
+                   sd_perChange = sd(percent_change)) 
+
+SiZerOuts_Summary_AvgPercentChange <- sig_only %>%
+  dplyr::group_by(LTER) %>% #add in stream if want to do it by stream
+  dplyr::summarize(ct=n(), 
+                   avg_perChange = mean(percent_change),
+                   sd_perChange = sd(percent_change)) 
+
 sig_only %>%
-  dplyr::group_by(dir_sig) %>%
+  dplyr::group_by(LTER, dir_sig) %>%
   dplyr::summarize(ct=n()) #n results in same thing as length. #be careful if number rows proxy looking for
 #can do length(Year) to double check. n function wants nothing in the parenthese
 
-#group by lter, chemical and stream and count number of unique sizer groups
-sig_only %>%
-  dplyr::group_by(LTER) %>%
-  summarize(sizer_ct = length(unique(sizer_groups))) #this will help identify breakpoints and inflection points
- 
-
+write.csv(SiZerOuts_Summary_Direction, file="SiZerOuts_Summary_Direction_Yield_Si_N.csv", row.names=FALSE)
+write.csv(SiZerOuts_Summary_AvgPercentChange, file="SiZerOuts_Summary_AvgPercentChange_Yield_Si_N.csv", row.names=FALSE)
 
 
