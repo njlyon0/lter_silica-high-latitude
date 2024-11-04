bb1f5ecd2d5f3466816886dd3f6af5704f349523
njlyon0 <nicholasjlyon@gmail.com>
2024-11-04 10:57:18 -0500

    feat: script for generating publication-quality summary tables

---
+++ 05_tables.R
@@ -0,0 +1,92 @@
+## ------------------------------------------------------- ##
+                      # Table Creation
+## ------------------------------------------------------- ##
+# Written by: Nick J Lyon
+
+# Purpose:
+## Make publication-quality tables
+
+# Pre-Requisites:
+## This script assumes you've run the following scripts:
+### - "02_stats-prep.R"
+### - "03a_stats.R"
+
+## ----------------------------------------- ##
+              # Housekeeping ----
+## ----------------------------------------- ##
+
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Make a folder for exporting graphs
+dir.create(path = file.path("tables"), showWarnings = F)
+
+# Clear environment
+rm(list = ls())
+
+## ----------------------------------------- ##
+# Stat Results Tables ----
+## ----------------------------------------- ##
+
+# Read in relevant table(s)
+mean_stats_v1 <- read.csv(file = file.path("stats_results", "avg_response_DSi_results.csv"))
+perc_stats_v1 <- read.csv(file = file.path("stats_results", "perc_change_DSi_results.csv"))
+
+# Check structure
+dplyr::glimpse(mean_stats_v1)
+dplyr::glimpse(perc_stats_v1)
+
+# Wrangle mean response
+mean_stats_v2 <- mean_stats_v1 %>% 
+  # Add response column
+  dplyr::mutate(Response = "Mean DSi Concentration (uM)") %>% 
+  # Strealine statistical information
+  dplyr::mutate(`F Statistic` = round(f_stat, digits = 1),
+                `P Value` = ifelse(round(p_value, digits = 3) == 0,
+                                   yes = "P < 0.0001",
+                                   no = as.character(round(p_value, digits = 3))) ) %>% 
+  # Clean up 'term' column
+  dplyr::mutate(term = gsub("scaled_mean_", "Scaled Mean ", term),
+                term = gsub("npp_kgC.m2.year", "NPP (kg C/m2/year)", term),
+                term = gsub("precip_mm.per.day", "Precipitation (mm/day)", term),
+                term = gsub("snow_max.prop.area", "Snow (Max Proportion Area)", term),
+                term = gsub("temp_degC", "Temperature (Celsius)", term),
+                term = gsub("P_Conc_uM", "P Concentration (uM)", term),
+                term = gsub("Discharge_cms", "Discharge (cms)", term) ) %>% 
+  # Pare down to only desired columns
+  dplyr::select(Response, Explanatory = term, `F Statistic`, `P Value`)
+
+# Check structure
+dplyr::glimpse(mean_stats_v2)
+
+# Tidy the percent change results in the same way
+perc_stats_v2 <- perc_stats_v1 %>% 
+  dplyr::mutate(Response = "Percent Change DSi Concentration") %>% 
+  dplyr::mutate(`F Statistic` = round(f_stat, digits = 1),
+                `P Value` = ifelse(round(p_value, digits = 3) == 0,
+                                   yes = "P < 0.0001",
+                                   no = as.character(round(p_value, digits = 3))) ) %>% 
+  dplyr::mutate(term = gsub("scaled_slope_", "Scaled Change in ", term),
+                term = gsub("npp_kgC.m2.year", "NPP (kg C/m2/year)", term),
+                term = gsub("precip_mm.per.day", "Precipitation (mm/day)", term),
+                term = gsub("snow_max.prop.area", "Snow (Max Proportion Area)", term),
+                term = gsub("temp_degC", "Temperature (Celsius)", term),
+                term = gsub("P_Conc_uM", "P Concentration (uM)", term),
+                term = gsub("Discharge_cms", "Discharge (cms)", term) ) %>% 
+  dplyr::select(Response, Explanatory = term, `F Statistic`, `P Value`)
+
+# Check that out
+perc_stats_v2
+
+# Combine the two data objects
+stats_out <- dplyr::bind_rows(mean_stats_v2, perc_stats_v2)
+
+# Final structure check
+dplyr::glimpse(stats_out)
+
+# Export
+write.csv(stats_out, na = '', row.names = F,
+          file = file.path("tables", "summary-table_statistical-results.csv"))
+
+# End ----
