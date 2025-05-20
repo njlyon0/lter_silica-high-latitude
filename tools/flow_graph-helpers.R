4171ff30313039e503114ad260e5b470f89f54a2
njlyon0 <nicholasjlyon@gmail.com>
2025-05-20 14:42:37 -0400

    feat (tools): shape palette for slope direction

--- tools/flow_graph-helpers.R
+++ tools/flow_graph-helpers.R
@@ -43,6 +43,7 @@ r2_palt <- c("great" = "#184e77", "good" = "#1a759f", "fine" = "#76c893",
              "bad" = "#b5e48c", "NA" = na_col)
 ## Slope direction palette
 dir_palt <- c("pos" = "#ff5400", "neg" = "#03045e", "NS" = nonsig_col, "NA" = na_col)
+dir_shps <- c("pos" = 24, "neg" = 25, "NS" = 22, "NA" = 21)
 ## Direction + P value palette
 dir_p_palt <- c("pos-sig" = "#ff5400", "pos-marg" = "#ff9e00", 
                 "neg-sig" = "#03045e", "neg-marg" = "#00b4d8",
