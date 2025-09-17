403e3e28687c7fec1910f29d647222b28d5024ef
njlyon0 <nicholasjlyon@gmail.com>
2025-09-17 11:04:40 -0400

    refactor (upload): combining `04x` and `05x` upload scripts

--- data-upload/04x_data-upload.R
+++ data-upload/04x_data-upload.R
@@ -7,7 +7,7 @@
 ## Upload data produced by script with same number prefix (e.g., "01_") to Google Drive
 
 # Pre-Requisites:
-## This script assumes you've run the corresponding "01_" script in the top-level of the repository
+## This script assumes you've run the corresponding "0#_" script in the top level of the repo
 
 ## ----------------------------------------- ##
 # Housekeeping ----
@@ -41,5 +41,34 @@ if(dir.exists(fig_path)){
                                                overwrite = T, path = fig_url))
   
 }
+
+# Clear environment + collect garbage
+rm(list = ls()); gc()
+
+## ----------------------------------------- ##
+# Upload Tables ----
+## ----------------------------------------- ##
+
+# Define path
+tab_path <- file.path("data", "tables")
+
+# Define Drive folder
+tab_url <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/15fBi0SAiA6cLsjjDymLezGgVcpCVO11o")
+
+# If outputs exist:
+if(dir.exists(tab_path)){
+  
+  # Identify local files
+  tab_local <- dir(path = tab_path)
+  
+  # Iterate across them uploading each
+  purrr::walk(.x = tab_local,
+              .f = ~ googledrive::drive_upload(media = file.path(tab_path, .x), 
+                                               overwrite = T, path = tab_url))
+  
+}
+
+# Clear environment + collect garbage
+rm(list = ls()); gc()
 
 # End ----
